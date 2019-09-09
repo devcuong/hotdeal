2c5b99565f5715d928e9c707e8fc5effaea77ea8
devcuong <cuong.dh8c@gmail.com>
2019-09-09 10:06:26 +0700

    update

--- website/js/main.js
+++ website/js/main.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
     var syncedSecondary = true;
     $("#slide-product").owlCarousel({
         loop: true,
+        margin: 20,
         nav: true,
+        dots: false,
+        navText: [
+            '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+            '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+        ],
         responsive: {
             0: {
                 items: 2.5

e645e1ee4de204c2d0cf0b3885b4107e95b4059e
devcuong <viettxt@gmail.com>
2019-09-09 22:52:17 +0700

    update

--- website/js/main.js
+++ website/js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
     var syncedSecondary = true;
+    $("#product-uytin").owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: false,
+        navText: [
+            '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+            '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 2.5
+            },
+            600: {
+                items: 2.5
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
     $("#slide-product").owlCarousel({
         loop: true,
         margin: 20,

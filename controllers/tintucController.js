9167b1df2b89108e68864531ac61d337a1075104
devcuong <viettxt@gmail.com>
2019-09-25 00:48:28 +0700

    update

--- controllers/tintucController.js
+++ controllers/tintucController.js
@@ -1,15 +1,14 @@
 const express = require("express");
 var router = express.Router();
 const mongoose = require("mongoose");
-const Truyen = mongoose.model("TinTuc");
+const tinTuc = mongoose.model("TinTuc");
 // const utils = require("../utils/navRender.js");
 // const stringHandle = require("../utils/stringHandle");
 // var moment = require('moment');
 
 router.get("/", (req, res) => {
     res.render("admin/quanLyTinTuc", {
-        // viewTitle: "Insert Truyện",
-        // timeStamp: tt
+
     });
 });
 

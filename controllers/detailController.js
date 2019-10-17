e57b37211d046e481349d601594e932c1049c2ef
devcuong <cuong.dh8c@gmail.com>
2019-10-18 01:04:36 +0700

    update

--- controllers/detailController.js
+++ controllers/detailController.js
@@ -2,6 +2,7 @@ const express = require("express");
 var router = express.Router();
 const mongoose = require("mongoose");
 const Truyen = mongoose.model("Truyen");
+const Chapter = mongoose.model("Chapter");
 router.get("/:slugTruyen", (req, res) => {
     var slugTruyen = req.params.slugTruyen;
     var q2 = Truyen.aggregate([{
@@ -27,8 +28,31 @@ router.get("/:slugTruyen", (req, res) => {
     });
 })
 router.get("/:slugTruyen/:tenChap", (req, res) => {
-    res.render("home/noiDungTrangChapter", {
-        layout: 'homeLayout.hbs'
+    var arrTenChap = req.params.tenChap.split("-");
+    var tenChap = arrTenChap[1];
+    Cha.aggregate([{
+            $lookup: { from: "chapter", localField: "_id", foreignField: "ma_truyen", as: "chap_moi_ra" }
+        },
+        // {
+        //     "$addFields": {
+        //         "chap_moi_ra": { "$slice": ["$chap_moi_ra", -3] }
+        //     }
+        // },
+        {
+            $lookup: { from: "theloai", localField: "the_loai", foreignField: "slug_the_loai", as: "ds_the_loai" }
+        },
+        { $match: { slug_truyen: req.params.slugTruyen } }
+        // ,
+        // {
+        //     $match: { "chap_moi_ra.ten_chuong": { $eq: arrTenChap[1] } }
+        // }
+    ]).exec(function(err, truyen) {
+        if (!err) {
+            console.log(truyen);
+            res.render("home/noiDungTrangChapter", {
+                layout: 'homeLayout.hbs'
+            })
+        }
     })
 })
 module.exports = router;

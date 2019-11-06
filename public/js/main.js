df344b347c3ef95a1bb23672467549a8fc3f7191
devcuong <cuong.dh8c@gmail.com>
2019-11-07 01:12:43 +0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -24,38 +24,4 @@ $(document).ready(function() {
             }
         }
     });
-    $("#slide-product").owlCarousel({
-        loop: true,
-        margin: 20,
-        nav: true,
-        dots: false,
-        navText: [
-            '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
-            '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0: {
-                items: 2.5
-            },
-            600: {
-                items: 2.5
-            },
-            1000: {
-                items: 3.5
-            }
-        }
-    });
-    $("#big").owlCarousel({
-        loop: true,
-        responsiveClass: true,
-        items: 1,
-        nav: true,
-        dots: true,
-        responsiveRefreshRate: 200,
-        navText: [
-            '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
-            '<i class="fa fa-arrow-right" aria-hidden="true"></i>'
-        ]
-    });
-
 });

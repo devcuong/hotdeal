0553ec2073edb144b6f0dacbecaec0dd685ba11f
devcuong <cuong.dh8c@gmail.com>
2019-11-11 09:48:31 +0700

    update

--- controllers/timTruyenController.js
+++ controllers/timTruyenController.js
@@ -6,10 +6,11 @@ const Chapter = mongoose.model("Chapter");
 const Error = mongoose.model("Error");
 var ObjectId = require('mongoose').Types.ObjectId;
 const dateFormat = require('dateformat');
-
+const stringHandle = require("../utils/stringHandle.js");
 // Lấy truyện theo thể loại
 router.get("/:theLoaiTruyen", (req, res) => {
     var theLoaiTruyen = req.params.theLoaiTruyen;
+    console.log(theLoaiTruyen);
     Truyen.find({ the_loai: theLoaiTruyen }).exec(function(err, truyen) {
         if (!err) {
             res.render("home/noiDungTrangTimTruyen", {
@@ -24,10 +25,24 @@ router.get("/:theLoaiTruyen", (req, res) => {
 // Lấy truyện theo keyword
 router.post("/tu-khoa/", (req, res) => {
     var tuKhoa = req.body.tuKhoa.trim();
-    Truyen.find( { 'ten_truyen' : { '$regex' : tuKhoa, '$options' : 'i' } } , { ten_truyen: 1, slug_truyen: 1 }).limit(20).exec(function(err, truyen){
+    Truyen.find( { 'slug_truyen' : { '$regex' : stringHandle.changeToSlug(tuKhoa), '$options' : 'i' } } , { ten_truyen: 1, slug_truyen: 1 }).limit(20).exec(function(err, truyen){
         if(!err){
             res.send(JSON.stringify(truyen));
         }
     })
+})
+
+// Lấy truyện theo theo keywork
+router.post("/ten-truyen/", (req, res) => {
+   var tuKhoa = req.body.tuKhoa.trim();
+   Truyen.find( { 'slug_truyen' : { '$regex' : stringHandle.changeToSlug(tuKhoa), '$options' : 'i' }}).exec(function(err, truyen){
+     if (!err) {
+       res.render("home/noiDungTrangTimTruyen", {
+           layout: 'homeLayout.hbs',
+           lstTruyen: truyen,
+           titleTrang: "DANH SÁCH TRUYỆN"
+       })
+     }
+   });
 })
 module.exports = router;

bfe412d25c494e8b541c06bd7cb9814487350cfe
devcuong <cuong.dh8c@gmail.com>
2019-11-13 23:08:48 +0700

    update

--- controllers/leechController.js
+++ controllers/leechController.js
@@ -1,13 +1,29 @@
 const express = require("express");
+var request = require("request");
 var router = express.Router();
 const mongoose = require("mongoose");
 const Truyen = mongoose.model("Truyen");
 
-// Lấy thông tin của truyện
+// Load trang leech truyện
 router.get("/", (req, res) => {
     res.render("admin/trangLeechTruyen", {
         layout: 'adminLayout.hbs',
         titleTrang: "LEECH TRANG TRUYỆN"
     })
+});
+
+// Lấy thông tin của truyện
+router.post("/", (req, res) => {
+    var urlTruyen = req.body.url;
+    var svTruyen = "http://chauau3.herokuapp.com/lay-truyen?id=" + urlTruyen;
+    request(
+        svTruyen,
+        function (error, response, body) {
+            if (error) {
+                return "lỗi";
+            } else {
+                res.json(body);
+            }
+        });
 });
 module.exports = router;

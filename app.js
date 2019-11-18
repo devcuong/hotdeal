9e2909cd7a1f66d28a221711a2ed61ce369cbc12
cuongdoan <viettxt@gmail.com>
2019-11-18 17:38:42 +0700

    update

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(bodyparser.json());
 app.set("views", path.join(__dirname, "/views/"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use('/images', express.static(__dirname + "/public/images"));
-app.use(express.compress());
 
 app.engine("hbs", exphbs({
     extname: "hbs",

efdb0694d74a7e56e914efe27c68163bab21eed1
devcuong <viettxt@gmail.com>
2019-11-21 21:10:39 +0700

    update

--- controllers/detailController.js
+++ controllers/detailController.js
@@ -105,7 +105,6 @@ function insertRecord(req, res) {
 // lấy list chapter của truyện
 router.post("/all-chapter", (req, res) => {
     var idTruyen = req.body.idTruyen;
-    console.log(idTruyen);
     var q = Chapter.find({ "ma_truyen": new ObjectId(idTruyen) }).sort({ ten_chuong: 1 }).collation({ locale: "en_US", numericOrdering: true });
     q.exec(function(err, docs) {
         if (!err) {

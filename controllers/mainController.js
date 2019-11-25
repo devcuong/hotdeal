acc96df6e318732d3e44a33268778ca127d91249
devcuong <viettxt@gmail.com>
2019-11-25 23:54:38 +0700

    update

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -12,7 +12,7 @@ const utils = require("../utils/navRender.js");
 // trang chu
 router.get("/:page*?", (req, res) => {
         var perPage = 12;
-        var page = req.params.page || 1;
+        var page = parseInt(req.params.page) || 1;
         var q2 = Truyen.aggregate([{
                 $lookup: { from: "chapter", localField: "_id", foreignField: "ma_truyen", as: "chap_moi_ra" }
             },

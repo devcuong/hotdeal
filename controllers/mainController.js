46be1e8ae83ae2e738d828eb728c7650f5b202f6
devcuong <viettxt@gmail.com>
2019-11-25 22:56:25 +0700

    update

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -30,7 +30,7 @@ router.get("/:page*?", (req, res) => {
                         lstTruyenCapNhat: truyens,
                         current: page,
                         pages: Math.ceil(count / perPage),
-                        navRender: utils.getNavRender(page, Math.ceil(count / perPage), "/the-loai/" + tl),
+                        navRender: utils.getNavRender(page, Math.ceil(count / perPage), "/"),
                         hostname: "http://truyenra.com"
                     });
                 });
